d32b61140cf07776e0ba3bab4c0c74275ccbc1ce
andrewcgaitskell <andrew@gaitskell.com>
2021-08-13 18:51:24 +0100

    Update 001_installpostgrs.sh

--- 001_installpostgrs.sh
+++ 001_installpostgrs.sh
@@ -4,8 +4,8 @@ sudo apt upgrade -y
 sudo apt install sudo
 
 sudo apt install -y software-properties-common
-sudo apt install -y postgresql-10
-sudo apt install -y postgresql-client-10
+sudo apt install -y postgresql
+sudo apt install -y postgresql-client
 
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
@@ -18,3 +18,6 @@ psqldb="data"   # Database name
 sudo printf "CREATE ROLE pythonuser WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION PASSWORD 'pythonuser';\nCREATE DATABASE $psqldb WITH OWNER $psqluser;" > createuserdb.sql
 
 sudo -u postgres psql -f createuserdb.sql
+
+# open up "firewall"
+# https://opensource.com/article/17/10/set-postgres-database-your-raspberry-pi
