3231f657253970f766914d514bffab98ad5e93dd
andrewcgaitskell <andrew@gaitskell.com>
2021-08-19 07:13:07 +0100

    Update copy_tcp_service.sh

--- copy_tcp_service.sh
+++ copy_tcp_service.sh
@@ -1,3 +1,9 @@
 sudo cp tcp_server.service /lib/systemd/system/tcp_server.service
 
 sudo chmod 644 /lib/systemd/system/tcp_server.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable tcp_server.service
+
+sudo systemctl status tcp_server.service
