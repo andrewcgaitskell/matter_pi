73c14aeccc7e5945b5c758b3fd43df95a7802ef9
andrewcgaitskell <andrew@gaitskell.com>
2021-08-22 19:00:08 +0100

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -30,7 +30,7 @@ while True:
 
     chunks = []
     while True:
-        content = client_sock.recv(3)
+        content = client_sock.recv(9)
         #print(type(content))
         if len(content) ==0:
            break
@@ -40,10 +40,23 @@ while True:
             sensorbytes = list(content)
             band = sensorbytes[0]
             sensorvalue = sensorbytes[1] * 256 + sensorbytes[2]
+            
+            mac0_string = sensorbytes[2].hex()
+            mac1_string = sensorbytes[3].hex()
+            mac2_string = sensorbytes[4].hex()
+            mac3_string = sensorbytes[5].hex()
+            mac4_string = sensorbytes[6].hex()
+            mac5_string = sensorbytes[7].hex()
+            mac6_string = sensorbytes[7].hex()
+            
+            full_mac = mac0_string + ":" + mac1_string + ":" + mac2_string + ":" + mac3_string + ":" + mac4_string + ":" + mac5_string + ":" + mac6_string
+            
+            
             ##sensorvalue = int.from_bytes(sensorcontet, byteorder='big')
             #print("band:", band, " sensor value:",sensorvalue)
             index = index + 1
-            sqlcmnd = f"INSERT INTO public.raw_sensordata(index, millitime, band, value) VALUES ({index}, EXTRACT(EPOCH FROM (SELECT NOW())) * 1000, {band}, {sensorvalue})"
+            sqlcmnd = f"INSERT INTO public.raw_sensordata_with_mac(index, millitime, band, value, mac)\
+            VALUES ({index}, EXTRACT(EPOCH FROM (SELECT NOW())) * 1000, {band}, {sensorvalue}, {full_mac})"
     
             #sqlcmnd = 'COPY "raw_CovidTrackerGantt" FROM \''+ filename + '\' DELIMITER \',\' CSV;'
             with engine.connect().execution_options(autocommit=True) as con:
