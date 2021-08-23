57d10116aa3c5e4b8ffde9e1161faa83f2e6df42
andrewcgaitskell <andrew@gaitskell.com>
2021-08-23 17:10:09 +0100

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -88,5 +88,5 @@ while True:
             #    pass
         #print(chunks)
         #client_sock.sendall(b''.join(chunks))
-        client_sock.close()
-        print("Disconnected from", client_addr)
+    client_sock.close()
+    print("Disconnected from", client_addr)
