3ef1ff25945bbe9d28b1838c57c80f1f2d2c4207
andrewcgaitskell <andrew@gaitskell.com>
2021-08-23 17:00:44 +0100

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -16,7 +16,7 @@ engine = create_engine('postgresql://pythonuser:pythonuser@localhost:5432/data')
 
 # Create server socket.
 serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-
+serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serv_sock.settimeout(1)
 
 
