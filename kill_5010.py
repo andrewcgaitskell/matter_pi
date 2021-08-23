b0aecbd708c5c9864b53e4694e4c09564a5c6697
andrewcgaitskell <andrew@gaitskell.com>
2021-08-23 16:54:38 +0100

    Delete kill_5010.py

--- kill_5010.py
+++
@@ -1,9 +0,0 @@
-import os
-import signal
-import subprocess
-
-command = "netstat -ano | findstr 5010"
-c = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr = subprocess.PIPE)
-stdout, stderr = c.communicate()
-pid = int(stdout.decode().strip().split(' ')[-1])
-os.kill(pid, signal.SIGTERM)
